3be45786755692f73544aff220e433b06c4ed947
NgocGiau11DHTTMT <ngoxgiau99@gmail.com>
2019-09-30 13:41:04 +0700

    11DHTTM-Tuan1

--- src/com/Dhttmt/tuan2/container.java
+++ src/com/Dhttmt/tuan2/container.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.Dhttmt.tuan2;
 
 import javax.swing.JPanel;
 
-/**
- *
- * @author HOME
- */
+
 class container {
 
     void add(JPanel pnBox) {
